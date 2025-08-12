46b4f8750f0b5da292f84644f48b81ef717302c7
erlbriton <telebite@yandex.ru>
2025-08-12 15:08:57 +0700

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,158 @@
+#include "Arduino.h"
+#include "core/options.h"
+#include "core/config.h"
+#include "core/telnet.h"
+#include "core/player.h"
+#include "core/display.h"
+#include "core/network.h"
+#include "core/netserver.h"
+#include "core/controls.h"
+#include "core/mqtt.h"
+#include "core/optionschecker.h"
+#include "core/timekeeper.h"
+#include <ModbusRTU.h>
+
+#define GPIO_OUT 32
+
+#if USE_OTA
+#if ESP_ARDUINO_VERSION >= ESP_ARDUINO_VERSION_VAL(3, 0, 0)
+#include <NetworkUdp.h>
+#else
+#include <WiFiUdp.h>
+#endif
+#include <ArduinoOTA.h>
+#endif
+
+#if DSP_HSPI || TS_HSPI || VS_HSPI
+SPIClass  SPI2(HOOPSENb);
+#endif
+
+extern __attribute__((weak)) void yoradio_on_setup();
+
+#if USE_OTA
+void setupOTA(){
+  if(strlen(config.store.mdnsname)>0)
+    ArduinoOTA.setHostname(config.store.mdnsname);
+#ifdef OTA_PASS
+  ArduinoOTA.setPassword(OTA_PASS);
+#endif
+  ArduinoOTA
+    .onStart([]() {
+      player.sendCommand({PR_STOP, 0});
+      display.putRequest(NEWMODE, UPDATING);
+      telnet.printf("Start OTA updating %s\n", ArduinoOTA.getCommand() == U_FLASH?"firmware":"filesystem");
+    })
+    .onEnd([]() {
+      telnet.printf("\nEnd OTA update, Rebooting...\n");
+      ESP.restart();
+    })
+    .onProgress([](unsigned int progress, unsigned int total) {
+      telnet.printf("Progress OTA: %u%%\r", (progress / (total / 100)));
+    })
+    .onError([](ota_error_t error) {
+      telnet.printf("Error[%u]: ", error);
+      if (error == OTA_AUTH_ERROR) {
+        telnet.printf("Auth Failed\n");
+      } else if (error == OTA_BEGIN_ERROR) {
+        telnet.printf("Begin Failed\n");
+      } else if (error == OTA_CONNECT_ERROR) {
+        telnet.printf("Connect Failed\n");
+      } else if (error == OTA_RECEIVE_ERROR) {
+        telnet.printf("Receive Failed\n");
+      } else if (error == OTA_END_ERROR) {
+        telnet.printf("End Failed\n");
+      }
+    });
+  ArduinoOTA.begin();
+}
+#endif
+
+ModbusRTU mb;
+uint16_t reg0 = 0;
+uint16_t prevReg0 = 0xFFFF; // Для отслеживания изменений
+
+void setup() {
+  Serial.begin(115200);
+  //----------------------Modbus--------------------------------------
+  Serial.println("ModbusRTU Slave стартует...");
+  pinMode(GPIO_OUT, OUTPUT);
+  digitalWrite(GPIO_OUT, LOW); // По умолчанию выключено
+  Serial2.begin(115200, SERIAL_8N1, 16, 17); // UART2: RX=GPIO16, TX=GPIO17
+//----------------------------------------------------------------------------
+  if(REAL_LEDBUILTIN!=255) pinMode(REAL_LEDBUILTIN, OUTPUT);
+  if (yoradio_on_setup) yoradio_on_setup();
+  pm.on_setup();
+  config.init();
+  display.init();
+  player.init();
+  network.begin();
+  if (network.status != CONNECTED && network.status!=SDREADY) {
+    netserver.begin();
+    initControls();
+    display.putRequest(DSP_START);
+    while(!display.ready()) delay(10);
+    return;
+  }
+  if(SDC_CS!=255) {
+    display.putRequest(WAITFORSD, 0);
+    Serial.print("##[BOOT]#\tSD search\t");
+  }
+  config.initPlaylistMode();
+  netserver.begin();
+  telnet.begin();
+  initControls();
+  display.putRequest(DSP_START);
+  while(!display.ready()) delay(10);
+  #ifdef MQTT_ROOT_TOPIC
+    mqttInit();
+  #endif
+  #if USE_OTA
+    setupOTA();
+  #endif
+  if (config.getMode()==PM_SDCARD) player.initHeaders(config.station.url);
+  player.lockOutput=false;
+  if (config.store.smartstart == 1) {
+    delay(250);
+    player.sendCommand({PR_PLAY, config.lastStation()});
+  }
+  pm.on_end_setup();
+  //---------------------------------------------------------------------------------
+  mb.begin(&Serial2, true); // true = режим обработки с аппаратным буфером (IRQ)
+  mb.slave(1); // Адрес slave = 1
+ // Добавляем Holding-регистр 0
+  mb.addHreg(0, reg0); // Изначально = 0
+}
+
+void loop() {
+  timekeeper.loop1();
+  telnet.loop();
+  if (network.status == CONNECTED || network.status==SDREADY) {
+    player.loop();
+#if USE_OTA
+    ArduinoOTA.handle();
+#endif
+  }
+  loopControls();
+  netserver.loop();
+  //----------------Modbus---------------------------------------
+ mb.task();
+
+  // Читаем текущее значение регистра 0
+  uint16_t val = mb.Hreg(0);
+
+  // Управляем GPIO32 в зависимости от значения
+  if (val == 1) {
+    digitalWrite(32, HIGH);
+  } else {
+    digitalWrite(32, LOW);
+  }
+  //---------------------Проверка UART------------------------------------------
+  // Serial2.print("9876543210");
+  // Serial2.print("\r\n"); // Перевод строки
+  // delay(1000); // Раз в секунду
+  // Serial2.print("012345678");
+  // Serial2.print("\r\n"); // Перевод строки
+  // delay(1000); // Раз в секунду
+}
+
+#include "core/audiohandlers.h"
