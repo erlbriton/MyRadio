775003576395191b1c10cce5574e11b387271a4e
erlbriton <telebite@yandex.ru>
2025-11-23 13:53:03 +0700

    Убрал лишние файлы из прошивки

--- myoptions.h
+++ myoptions.h
@@ -4,10 +4,8 @@
 #define DSP_MODEL			DSP_ILI9488
 #define TFT_DC			    22
 #define TFT_CS			    4
-//#define I2S_INTERNAL		true
-//#define IR_PIN			21
 #define I2S_DOUT			27
 #define I2S_BCLK			26
 #define I2S_LRC			    25
-//#define IR_PIN			34
+
 #endif
